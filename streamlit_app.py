c7282214a66617c4f5faa53c496c0f4e4e791f65
Robert.D.Apple <31928153+Rob4Hope@users.noreply.github.com>
2022-09-23 09:00:21 -0600

    Update streamlit_app.py
    Made it look like page

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("OMega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 OMega 3 & Blueberry Oatmeal")
+streamlit.text("🥬 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avacado Toast")
+
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothy 🥝🍇")
