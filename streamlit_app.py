365b9a60ec97db2fee8699c2d3806cfa1320a43f
Robert.D.Apple <31928153+Rob4Hope@users.noreply.github.com>
2022-09-25 09:46:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #--Displays the dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+#--Challenge lab
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
+streamlit.header("The fruit load list contains:")
+
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
